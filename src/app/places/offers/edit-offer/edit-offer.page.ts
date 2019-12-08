55e4395b3606058b2c7efae19964ba6752085012
kingkoor <kingkoor@yahoo.com>
2019-12-08 13:27:26 -0500

    edit page complete

--- src/app/places/offers/edit-offer/edit-offer.page.ts
+++ src/app/places/offers/edit-offer/edit-offer.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { NavController } from '@ionic/angular';
-
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Place } from '../../place.model';
 import { PlacesService } from '../../places.service';
 
@@ -12,6 +12,7 @@ import { PlacesService } from '../../places.service';
 })
 export class EditOfferPage implements OnInit {
   place: Place;
+  form: FormGroup;
   constructor(
     private route: ActivatedRoute,
     private navCtrl: NavController,
@@ -25,7 +26,23 @@ export class EditOfferPage implements OnInit {
         return;
       }
       this.place = this.placesService.getPlace(paramMap.get('placeId'));
+      this.form = new FormGroup({
+        title: new FormControl(this.place.title, {
+          updateOn: 'blur',
+          validators: [Validators.required]
+        }),
+        description: new FormControl(this.place.description, {
+          updateOn: 'blur',
+          validators: [Validators.required, Validators.maxLength(180)]
+        })
+      });
     });
+  }
+  onUpdateOffer() {
+    if (!this.form.valid) {
+      return;
+    }
+    console.log(this.form);
   }
 
 
