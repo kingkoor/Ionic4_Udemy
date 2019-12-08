c30306d0e8be174f418f4037d0f0c18d14d3ef32
kingkoor <kingkoor@yahoo.com>
2019-12-08 09:31:45 -0500

    place detail actionsheet controller

--- src/app/places/discover/place-detail/place-detail.page.ts
+++ src/app/places/discover/place-detail/place-detail.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 // import { Router } from '@angular/router';
-import { NavController, ModalController } from '@ionic/angular';
+import { NavController, ModalController, ActionSheetController } from '@ionic/angular';
 import { ActivatedRoute } from '@angular/router';
 
 import { Place } from '../../place.model';
@@ -17,7 +17,8 @@ export class PlaceDetailPage implements OnInit {
   constructor(private navCtrl: NavController,
               private route: ActivatedRoute,
               private placesService: PlacesService,
-              private modalCtrl: ModalController) {}
+              private modalCtrl: ModalController,
+              private actionsheetCtrl: ActionSheetController) {}
 
   ngOnInit() {
     this.route.paramMap.subscribe(paramMap => {
@@ -32,6 +33,32 @@ export class PlaceDetailPage implements OnInit {
     // this.router.navigateByUrl('/places/tabs/discover');
     // this.navCtrl.navigateBack('/places/tabs/discover');
     // this.navCtrl.pop();
+    this.actionsheetCtrl.create({
+      header: 'Choose an Action',
+      buttons: [
+        {
+          text: 'Select Date',
+          handler: () => {
+            this.openBookingModal('select');
+          }
+        },
+        {
+          text: 'Random Date',
+          handler: () => {
+            this.openBookingModal('random');
+          }
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel'
+        }
+      ]
+    }).then( actionSheetEl => {
+      actionSheetEl.present();
+    });
+  }
+  openBookingModal( mode: 'select' | 'random') {
+    console.log ( mode);
     this.modalCtrl
     .create({component: CreateBookingComponent,
       componentProps: {selectedPlace: this.place}
@@ -45,6 +72,7 @@ export class PlaceDetailPage implements OnInit {
         console.log('BOOKED !');
       }
     });
+
   }
 
 }
