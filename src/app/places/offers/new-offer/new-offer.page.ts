242193602c38711f07648192929e73c05fa540f4
kingkoor <kingkoor@yahoo.com>
2019-12-08 15:23:46 -0500

    new booking is not showing

--- src/app/places/offers/new-offer/new-offer.page.ts
+++ src/app/places/offers/new-offer/new-offer.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { PlacesService } from '../../places.service';
 
 @Component({
   selector: 'app-new-offer',
@@ -9,7 +12,7 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 export class NewOfferPage implements OnInit {
   form: FormGroup;
 
-  constructor() {}
+  constructor(private placesService: PlacesService, private router: Router) {}
 
   ngOnInit() {
     this.form = new FormGroup({
@@ -37,6 +40,18 @@ export class NewOfferPage implements OnInit {
   }
 
   onCreateOffer() {
-    console.log(this.form);
+    if (!this.form.valid) {
+      return;
+    }
+    this.placesService.addPlace(
+      this.form.value.title,
+      this.form.value.description,
+      +this.form.value.price,
+      new Date(this.form.value.dateFrom),
+      new Date(this.form.value.dateTo)
+    );
+    console.log(this.placesService.places);
+    this.form.reset();
+    this.router.navigate(['/places/tabs/offers']);
   }
 }
