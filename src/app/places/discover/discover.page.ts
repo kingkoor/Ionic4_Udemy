294615ac59d28def54965898787dd30e3d88e071
kingkoor <kingkoor@yahoo.com>
2019-12-07 22:22:02 -0500

    Loading controller auth and other items

--- src/app/places/discover/discover.page.ts
+++ src/app/places/discover/discover.page.ts
@@ -1,22 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular';
+import { SegmentChangeEventDetail } from '@ionic/core';
+
 import { PlacesService } from '../places.service';
-// import { MenuController } from '@ionic/angular';
 import { Place } from '../place.model';
 
 @Component({
   selector: 'app-discover',
   templateUrl: './discover.page.html',
-  styleUrls: ['./discover.page.scss'],
+  styleUrls: ['./discover.page.scss']
 })
 export class DiscoverPage implements OnInit {
   loadedPlaces: Place[];
-  constructor(private placesService: PlacesService) { }
+  listedLoadedPlaces: Place[];
+
+  constructor(
+    private placesService: PlacesService,
+    private menuCtrl: MenuController
+  ) {}
 
   ngOnInit() {
     this.loadedPlaces = this.placesService.places;
+    this.listedLoadedPlaces = this.loadedPlaces.slice(1);
   }
-  // onOpenMenu() {
-  //   this.menuCtrl.toggle();
-  // }
 
+  onOpenMenu() {
+    this.menuCtrl.toggle();
+  }
+
+  onFilterUpdate(event: CustomEvent<SegmentChangeEventDetail>) {
+    console.log(event.detail);
+  }
 }
