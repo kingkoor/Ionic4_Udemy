6354661f899bf841a3ceae81f2307622642fd493
kingkoor <kingkoor@yahoo.com>
2019-12-10 18:30:53 -0500

    final before reinstall

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,26 +1,49 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+
+interface AuthResponseData {
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  localId: string;
+  expiresIn: string;
+  registered?: boolean;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-// tslint:disable-next-line: variable-name
-private _userIsAuthenticated = false;
-// tslint:disable-next-line: variable-name
-private _userId = 'xyz';
+  // tslint:disable-next-line: variable-name
+  private _userIsAuthenticated = false;
+  // tslint:disable-next-line: variable-name
+  private _userId = null;
 
-get userIsAuthenticated() {
-  return this._userIsAuthenticated;
-}
-get userId() {
-  return this._userId;
-}
+  get userIsAuthenticated() {
+    return this._userIsAuthenticated;
+  }
 
-  constructor() { }
+  get userId() {
+    return this._userId;
+  }
+
+  constructor(private http: HttpClient) {}
+
+  signup(email: string, password: string) {
+    return this.http.post<AuthResponseData>(
+      `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${
+        environment.firebaseAPIKey
+      }`,
+      // tslint:disable-next-line: object-literal-shorthand
+      { email: email, password: password, returnSecureToken: true }
+    );
+  }
 
   login() {
     this._userIsAuthenticated = true;
   }
+
   logout() {
     this._userIsAuthenticated = false;
   }
